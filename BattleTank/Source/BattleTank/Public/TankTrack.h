614a13c413b193363e5a232350b725f5797ab119
Daniel-PC\Daniel <gudanielf@gmail.com>
2017-03-19 16:53:40 -0300

    ApplyingForceAtLocation() para começar a movimentação do tanque

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -17,5 +17,7 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Custom")
 	void SetThrottle(float Throttle);
 	
-	
+	//Max force per track in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 500000; // assum tank at 1g acceleration (10m/s2)
 };
